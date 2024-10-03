e9f1a8dc9841d03734547a86a98c902a97eac0ac
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2024-10-03 19:06:07 -0300

    implementa run() em Cliente

--- Ecommerce/src/Cliente.java
+++ Ecommerce/src/Cliente.java
@@ -9,4 +9,40 @@ public class Cliente implements Runnable {
         this.clienteId = clienteId;
         this.filaDePedidos = filaDePedidos;
     }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                List<Item> itens = new ArrayList<>();
+                int quantidadeA = random.nextInt(5) + 1;
+                int quantidadeB = random.nextInt(5) + 1;
+
+                boolean adicionouProdutoA = random.nextBoolean();
+                boolean adicionouProdutoB = random.nextBoolean();
+
+                if (adicionouProdutoA) {
+                    itens.add(new Item("ProdutoA", quantidadeA));
+                }
+                if (adicionouProdutoB) {
+                    itens.add(new Item("ProdutoB", quantidadeB));
+                }
+                if (!adicionouProdutoA && !adicionouProdutoB) {
+                    itens.add(new Item("ProdutoA", quantidadeA));
+                }
+
+                Pedido pedido = new Pedido(clienteId, itens);
+                filaDePedidos.put(pedido);
+
+                System.out.println("Cliente " + clienteId + " enviou um pedido de " +
+                        (itens.stream().map(item -> item.getNomeProduto() + ": " + item.getQuantidade())
+                                .reduce((a, b) -> a + ", " + b).orElse("Nenhum produto")));
+
+                Thread.sleep(random.nextInt(2000) + 1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 }

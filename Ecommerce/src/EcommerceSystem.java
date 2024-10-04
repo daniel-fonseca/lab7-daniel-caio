83653b7ebecc04bbdb1b0d77eb8199a38478a2fd
Caio Victor <caio.araujo@ccc.ufcg.edu.br>
2024-10-03 21:07:42 -0300

    criação das threads

--- Ecommerce/src/EcommerceSystem.java
+++ Ecommerce/src/EcommerceSystem.java
@@ -23,4 +23,49 @@ public class ECommerceSystem {
             executor.submit(new Worker(i + 1, filaDePedidos, filaDeEspera, estoque, pedidosProcessados, pedidosRejeitados, valorTotalVendas));
         }
 
-    }
+
+        executor.submit(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    Thread.sleep(10000);
+                    reabastecerEstoque();
+                    reprocessarPedidosPendentes();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+
+
+        executor.submit(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    Thread.sleep(30000);
+                    gerarRelatorio();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+
+        try {
+            Thread.sleep(120000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("Iniciando o shutdown do sistema...");
+        executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
+
+        System.out.println("Sistema encerrado.");
+
+
+}
